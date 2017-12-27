bd988f3533d30f933014de382b3356c798198ec7
Philippe Brossier <brossierp@gmail.com>
2017-12-27 22:10:06 +0000

    1st commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Project started using https://start.spring.io/
+
+
+To build:
+	- upgrade your Gradle Wrapper to the latest version 4.4.1:
+		./gradlew wrapper --gradle-version=4.4.1 --distribution-type=bin
+	- to build:
+		./gradlew clean build
+
+
+To run:
+	- TODO
+	
---
+++ src/main/java/com/example/gradleAndBoot/GradleAndBootApplication.java
@@ -0,0 +1,12 @@
+package com.example.gradleAndBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleAndBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleAndBootApplication.class, args);
+	}
+}
